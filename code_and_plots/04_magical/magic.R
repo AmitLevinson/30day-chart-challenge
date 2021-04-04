32a4d4c486ddfe7127a794352e8533d1893a9afa
AmitLevinson <amitlevinson1@gmail.com>
2021-04-04 23:32:21 +0300

    increase subtitle

--- code_and_plots/04_magical/magic.R
+++ code_and_plots/04_magical/magic.R
@@ -39,23 +39,23 @@ time_referenced <- cmnts %>%
 time_referenced %>% 
   ggplot(aes(x = time_numeric))+
   geom_histogram(bins = 50, fill = "gray35", color = NA )+
-  scale_x_continuous(name = "Video length (minutes)\n", breaks = seq(0,840, 120), labels = c("0:00", seq(2,12,2), "14:00"))+
+  scale_x_continuous(name = "\n Minute\n", breaks = seq(0,840, 120), labels = c("0:00", seq(2,12,2), "14:00"))+
   labs(title = "Highlights of a Magic Trick Video <span style='font-family: \"Font Awesome 5 Free Solid\"'>&#xf0d0;</span>",
-       subtitle = "Bars represent frequency of comments referencing a time frame in a magick trick video. The analysis is based on ~18,000\ncomments from the video \"BEST Magic Show in the world - Genius Rubik\'s Cube Magician America\'s Got Talent\" on YouTube",
+       subtitle = "Bars represent frequency of comments referencing a time-frame in a magick trick video. The analysis is\nbased on ~18,000 comments from the video \"BEST Magic Show in the world - Genius Rubik\'s Cube\nMagician America\'s Got Talent\" on YouTube",
        y = "Number of references",
        caption = "Data: YouTube | Viz: Amit_Levinson")+
   theme(
     text = element_text(family = "Leelawadee UI"),
     plot.title = element_markdown(family = "Quite Magical", size = 24),
-    plot.subtitle = element_text(size = 10),
+    plot.subtitle = element_text(size = 11),
     plot.caption = element_text(color = "gray25", size = 8, hjust = 0),
-    axis.title = element_text(color = "gray25", size = 11),
-    axis.text = element_text(color = "gray10", size = 9),
+    axis.title = element_text(color = "gray25", size = 12),
+    axis.text = element_text(color = "gray10", size = 10),
     axis.ticks = element_blank(),
     panel.grid = element_blank(),
     panel.background = element_rect(fill = "#ffe9ec"),
     plot.background = element_rect(fill = "#ffe9ec"),
-    plot.margin = margin(4,2,4,2, "mm")
+    plot.margin = margin(6,4,4,4, "mm")
   )
 
 ggsave("code_and_plots/04_magical/magic.png", height = 7, width = 9)
