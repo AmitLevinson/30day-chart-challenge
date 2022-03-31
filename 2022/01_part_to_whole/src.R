55a5107c3bd125bc152bd0122e6d72f4e9b42b83
AmitLevinson <amitlevinson1@gmail.com>
2022-03-31 21:49:44 +0300

    update chart

--- 2022/01_part_to_whole/src.R
+++ 2022/01_part_to_whole/src.R
@@ -19,15 +19,18 @@ clean_script <- function (script) {
 
 scripts <- map_dfr(file_path, clean_script, .id = "rowid")
 
-scripts %>% 
+agg_packages <- scripts %>% 
   mutate(total_scripts = max(as.numeric(rowid))) %>% 
   group_by(package) %>% 
   summarise(
     n = n(),
     pct = n / total_scripts,
     total_scripts = max(total_scripts),
+    .groups = "drop"
   ) %>% 
   arrange(-n) %>% 
   distinct(package, n, pct, total_scripts)
+
+write.csv(agg_packages, "packages.csv")
 
 
--- 2022/01_part_to_whole/src.js
+++ 2022/01_part_to_whole/src.js
@@ -1,25 +1,13 @@
+// Code adapted from
+// http://bl.ocks.org/jadiehm/7cc6b821d3df14280f33
 
-
-
-// Both are valid
-// d3.csv("packages.csv").then(function(data) {
-
-//     data.forEach(function(d) {
-//         d.n = +d.n;
-//         d.pct = +d.pct;
-//         d.total_scripts = +d.total_scripts;
-//       });
-    
-//     cleandat = data.slice(0,10)
-//     console.table(cleandat)
-
-//   });
-    
+async function drawChart () {
+  
   const margin = {top: 10, right: 50, bottom: 20, left: 50},
   width = 550 - margin.right - margin.left;
   height = 250 - margin.top - margin.bottom;
   var barHeight = 20;    
-
+  
   var svg = d3.select("#wrapper")
     .append("svg")
     .attr('preserveAspectRatio', "xMinYMid meet")
@@ -28,7 +16,6 @@
   bounds = svg.append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-async function drawChart () {
 
   const data = await d3.csv("packages.csv");
   data.forEach(function(d) {
@@ -63,12 +50,7 @@ console.log([cleandat.map(d => d.package)])
     .scale(xScale)
 
   var formatPct = d3.format(".0%")
-    // .tickFormat(d => (d == 0) ? d + "%" : d * 100 + "%")
-  
-  // svg.append("g")
-  //   .attr("class", "x axis")
-  //   // .attr("transform", "translate(0," + height + ")")
-  //   .call(xAxis)
+
 
   // bind data to the bars
   var categoryGroup = svg.selectAll('.g-categories')
@@ -92,10 +74,9 @@ console.log([cleandat.map(d => d.package)])
     .attr("width", function(d) {return xScale(d.pct);})
     .attr("height", barHeight)
     .attr('class', 'g-num')
-    // .attr("transform", "translate(0,4)")
-    
-    // Background bars
-    
+
+
+ 
     var labelGroup = svg.selectAll("g-num")
       .data(cleandat)
       .enter()
@@ -106,15 +87,6 @@ console.log([cleandat.map(d => d.package)])
         .attr("x", d => xScale(0.001))
         .attr("y", d => yScale(d.package))
         .attr("class", "g-labels")
-    // svg.selectAll('.g-categories')
-    // .data(cleandat)
-    // .enter()
-    // .append('text')
-    // .attr("transform", "translate(0,15)")
-    // .style("fill", 'white')
-    // .text(d => d.package)
-    console.log(labelGroup)
-    // categoryGroup.append("text")
       
 
     d3.select(window).on("resize", function() {
@@ -127,5 +99,17 @@ console.log([cleandat.map(d => d.package)])
 
 drawChart();
 
-// Help from
-// http://bl.ocks.org/jadiehm/7cc6b821d3df14280f33
+
+// d3.csv("packages.csv").then(function(data) {
+
+//     data.forEach(function(d) {
+//         d.n = +d.n;
+//         d.pct = +d.pct;
+//         d.total_scripts = +d.total_scripts;
+//       });
+    
+//     cleandat = data.slice(0,10)
+//     console.table(cleandat)
+
+//   });
+    
